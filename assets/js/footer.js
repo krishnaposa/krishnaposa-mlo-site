b7cacd7d6e5bd72a57ed525265605a2c406ad59a
krishnaposa <krishna.posa@gmail.com>
2025-08-21 13:17:45 -0400

    Create footer.js

---
+++ assets/js/footer.js
@@ -0,0 +1,6 @@
+fetch("partials/footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  })
+  .catch(error => console.error("Error loading footer:", error));
