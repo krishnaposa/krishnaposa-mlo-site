80a1534eccf0175abc0e3c1bc960a8a3a3390687
krishnaposa <krishna.posa@gmail.com>
2025-09-17 08:13:26 -0400

    update invest-analyzer files

--- scripts/property-analyzer/finance.py
+++
@@ -1,6 +0,0 @@
-# finance.py
-def mortgage_pi(price, down, rate_pct, years):
-    loan = price - down
-    r = rate_pct/100/12
-    n = years*12
-    return round(loan * r * (1+r)**n / ((1+r)**n - 1), 2)
