1a245c511d80e8d7cbd33640186dc22b858fd01d
krishnaposa <krishna.posa@gmail.com>
2025-09-17 05:23:05 -0400

    Create health.py

---
+++ azure/functions/rent-analyzer-fn/routes/health.py
@@ -0,0 +1,9 @@
+# routes/health.py
+from app import app
+import azure.functions as func
+from utils.common import cors_headers
+
+@app.function_name(name="health")
+@app.route(route="health", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
+def health(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse('{"ok": true}', mimetype="application/json", headers=cors_headers())
