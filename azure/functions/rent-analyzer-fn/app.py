a2415b3e04099e99a18321b4723c9aa626bc983f
krishnaposa <krishna.posa@gmail.com>
2025-09-17 05:42:23 -0400

    Update app.py

--- azure/functions/rent-analyzer-fn/app.py
+++ azure/functions/rent-analyzer-fn/app.py
@@ -1,5 +1,4 @@
-# app.py
 import azure.functions as func
 app = func.FunctionApp()
 
-from routes import health, rent_prefetch, rent_analyze, tax_estimate  # <-- add tax_estimate
+from routes import health, rent_prefetch, rent_analyze, expense_all  # add expense_all
