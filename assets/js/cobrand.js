1c2e362f3f0264c6ae2290e1c740054a52393cc1
krishnaposa <krishna.posa@gmail.com>
2025-09-18 18:59:55 -0400

    updates for serene

--- assets/js/cobrand.js
+++ assets/js/cobrand.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <img src="https://www.krishposa.com/assets/img/headshot.jpg" alt="Krish Posa">
         <span>Mortgage by <strong>Krish Posa</strong></span>
         <span>|</span>
-        <img src="https://www.krishposa.com/assets/img/brokerages/gvr.JPG" alt="GVR Realty LLC logo">
+        <img src="https://www.krishposa.com/assets/img/realtors/gvr.JPG" alt="GVR Realty LLC logo">
         <span>In partnership with <strong>GVR Realty LLC</strong></span>
       </div>
     </div>`;
